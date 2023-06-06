54b75f001ce8b2354704d753c66946f527dee851
Mariyam Rahman <mariyamrahman38151@gmail.com>
2023-06-06 23:55:29 +0600

    First page completd

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { useEffect, useState } from "react";
+import { Card, Col, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+const App = () => {
+  const [shows, setShows] = useState([]);
+  useEffect(() => {
+    fetch("https://api.tvmaze.com/search/shows?q=all")
+      .then((res) => res.json())
+      .then((data) => setShows(data));
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Row className="g-4 row">
+        {shows.map((show, idx) => (
+          <Col key={idx} className="col-4" xl={3}>
+            <Card className="">
+              <Card.Img
+                variant="top"
+                src={show.show.image.original}
+                className=""
+                style={{ aspectRatio: "2/3", objectFit: "cover" }}
+              />
+              <Card.Body>
+                <Card.Title>{show.show.name}</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+              <Button variant="primary">View Details</Button>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
 
-export default App
+export default App;
